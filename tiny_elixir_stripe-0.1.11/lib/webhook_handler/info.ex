0a3bdeb59bde3ba7d0a59de1433af3ba92299f4b
Eileen Noonan <enoonan@arcstone.com>
2025-12-19 07:49:41 -0500

    chore: clean up artifacts

--- tiny_elixir_stripe-0.1.11/lib/webhook_handler/info.ex
+++
@@ -1,9 +0,0 @@
-defmodule TinyElixirStripe.WebhookHandler.Info do
-  @moduledoc """
-  Introspection functions for webhook handler DSL.
-  """
-
-  use Spark.InfoGenerator,
-    extension: TinyElixirStripe.WebhookHandler.Dsl,
-    sections: [:handlers]
-end
